c8e5f8acb57bb673e4490bfc8b2d3bbc7ce7fdc5
Ruizhi Wang <ruizhi@ruizhis-mbp.home>
2019-12-08 19:17:46 +0000

    reset website to clean start with venv

--- hello/views.py
+++ hello/views.py
@@ -4,19 +4,6 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
-# def index(request):
-#     return HttpResponse('Noemie & Ruizhi - Coming soon!')
-#     return render(request, "index.html")
-
 def index(request):
-    return render(request, 'home.html')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
