17fdbd5f0927309dfc4e549f132614efb7a568a9
Ruizhi Wang <ruizhi@Ruizhis-MacBook-Pro.local>
2019-12-22 17:18:23 +0100

    gunicorn settings fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn bigday.wsgi --log-file -
--- bigday/wsgi.py
+++ bigday/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for bigday project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
