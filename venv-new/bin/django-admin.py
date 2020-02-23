f0632dc14032e88611e5b162eaed32b66f0d8060
Ruizhi Wang <ruizhi@ruizhis-mbp.home>
2020-02-23 18:02:39 +0000

    reset venv

---
+++ venv-new/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ruizhi/Documents/Wedding - Google Drive/website/wedding_website/venv-new/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
