ada818a1ee226e24676ab98480752922c6f8e33e
Ruizhi Wang <ruizhi@ruizhis-mbp.home>
2020-04-05 10:18:19 +0100

    Email form removed

--- bigday/settings.py
+++ bigday/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-ADMINS = config('ADMINS', cast=Csv())
 
 
 # Application definition
